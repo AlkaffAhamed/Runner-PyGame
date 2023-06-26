d78cba3eb0f5baaea37243d26359cd250332628c
AlkaffAhamed <alkaffahamed@hotmail.com>
2023-06-26 14:13:13 +0800

    06 Create Text Surface 1. Create the font object: `test_font = pygame.font.Font("font/Pixeltype.ttf", 50)` 2. Render the text (or Create the text Surface): `text_surface = test_font.render("Runner PyGame", False, 'Black')` 3. Blit the surface inside the game loop

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner PyGame")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 # test_surface = pygame.Surface((100, 200))
 # test_surface.fill((255, 0, 0)) # or test_surface.fill("Red")
 sky_surface = pygame.image.load("graphics/Sky.png")
 ground_surface = pygame.image.load("graphics/ground.png")
+text_surface = test_font.render("Runner PyGame", False, 'Black')
 
 # Game loop
 while True:
@@ -24,6 +26,7 @@ while True:
     # screen.blit(test_surface, (200, 100))
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 30))
 
     # Draw all our elements
     # Update everything
