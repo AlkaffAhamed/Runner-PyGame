d54ef21db9247f652d261462b5b10551d4fb219a
AlkaffAhamed <alkaffahamed@hotmail.com>
2023-06-27 18:00:17 +0800

    23 Game Over State 1. Create new variable `game_active = True` 2. Inside game loop, only do everything if `game_active = True` 3. The Game Over logic should be in the `else` part 4. Inside event loop, reset game if Space key down 5. Remember to reset snail position also

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner PyGame")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+game_active = True
 
 # test_surface = pygame.Surface((100, 200))
 # test_surface.fill((255, 0, 0)) # or test_surface.fill("Red")
@@ -34,77 +35,96 @@ while True:
             # To prevent the error
             # pygame.error: video system not initialized
             sys.exit()
-        # Mouse event using event loop
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos):
-        #         print("MOUSEOVER - EVENT")
-        # if event.type == pygame.MOUSEBUTTONUP:
-        #     print("Mouse-UP")
-        # if event.type == pygame.MOUSEBUTTONDOWN:
-        #     print("Mouse-DOWN")
 
-        # Keyboard event using event loop
-        # if event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_SPACE:
-        #         print("JUMP! - EVENT")
-        # if event.type == pygame.KEYUP:
-        #     print("Key-UP")
+        # Not Game Over State
+        if game_active:
+            # Mouse event using event loop
+            # if event.type == pygame.MOUSEMOTION:
+            #     if player_rect.collidepoint(event.pos):
+            #         print("MOUSEOVER - EVENT")
+            # if event.type == pygame.MOUSEBUTTONUP:
+            #     print("Mouse-UP")
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     print("Mouse-DOWN")
 
-        # Jump when Space is pressed
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-                player_gravity = -20
+            # Keyboard event using event loop
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_SPACE:
+            #         print("JUMP! - EVENT")
+            # if event.type == pygame.KEYUP:
+            #     print("Key-UP")
 
-        # Jump when player is clicked
-        if event.type == pygame.MOUSEBUTTONUP:
-            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
-                player_gravity = -20
+            # Jump when Space is pressed
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
 
-    # screen.blit(test_surface, (200, 100))
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
+            # Jump when player is clicked
+            if event.type == pygame.MOUSEBUTTONUP:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                    player_gravity = -20
+        # Game Over State
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
 
-    # Score Text
-    pygame.draw.rect(screen, "#C0E8EC", score_rect)  # Fill
-    # pygame.draw.rect(screen, "#C0E8EC", score_rect, 10,6)  # Border/Margins
-    screen.blit(score_surface, score_rect)
+    # Not Game Over State
+    if game_active:
+        # screen.blit(test_surface, (200, 100))
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, 300))
 
-    # Snail movement
-    # Important to draw the background first before drawing other objects
-    snail_rect.x -= 4
-    if snail_rect.left < 0:
-        snail_rect.right = 800
-    screen.blit(snail_surface, snail_rect)
+        # Score Text
+        pygame.draw.rect(screen, "#C0E8EC", score_rect)  # Fill
+        # pygame.draw.rect(screen, "#C0E8EC", score_rect, 10,6)  # Border/Margins
+        screen.blit(score_surface, score_rect)
 
-    # Keys using the key module
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    #     print("JUMP!")
+        # Snail movement
+        # Important to draw the background first before drawing other objects
+        snail_rect.x -= 4
+        if snail_rect.left < 0:
+            snail_rect.right = 800
+        screen.blit(snail_surface, snail_rect)
 
-    # Move the Player by moving the rectangle that contains the player
-    # player_rect.left += 2
+        # Keys using the key module
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_SPACE]:
+        #     print("JUMP!")
 
-    # Player Gravity
-    player_gravity += 1
-    player_rect.y += player_gravity
+        # Move the Player by moving the rectangle that contains the player
+        # player_rect.left += 2
 
-    # To prevent the player from falling below ground
-    if player_rect.bottom >= 300:
-        player_rect.bottom = 300
+        # Player Gravity
+        player_gravity += 1
+        player_rect.y += player_gravity
 
-    screen.blit(player_surface, player_rect)
+        # To prevent the player from falling below ground
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
 
-    # Collision Detection with Snail
-    # r1.colliderect(r2)
-    # if player_rect.colliderect(snail_rect):
-    #     print("COLLISION!")
+        screen.blit(player_surface, player_rect)
 
-    # Collision Detection with Mouse
-    # r1.collidepoint((x,y))
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print("MOUSEOVER!")
-    #     print(pygame.mouse.get_pressed())
+        # Collision Detection with Snail
+        # r1.colliderect(r2)
+        # if player_rect.colliderect(snail_rect):
+        #     print("COLLISION!")
+
+        # Collision Detection with Mouse
+        # r1.collidepoint((x,y))
+        # mouse_pos = pygame.mouse.get_pos()
+        # if player_rect.collidepoint(mouse_pos):
+        #     print("MOUSEOVER!")
+        #     print(pygame.mouse.get_pressed())
+
+        # Check for Game Over State
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+
+    # Game Over State
+    else:
+        # Game Over Logic
+        screen.fill("Yellow")
 
     # Draw all our elements
     # Update everything
